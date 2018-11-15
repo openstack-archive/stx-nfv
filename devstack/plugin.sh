c3b59366a66e09826d2475868e3ed649323cc97c
Dean Troyer <dtroyer@gmail.com>
2018-11-15 09:49:31 -0600

    Updates to DevStack plugin to install guest-client
    Add a basic DevStack job to exercise this
    
    Change-Id: I5b079eacebf9ddb433b5e831d557afe6a1be6880
    Signed-off-by: Dean Troyer <dtroyer@gmail.com>

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,17 +1,21 @@
-# Initial source of lib script
-source $DEST/stx-nfv/devstack/lib/stx-nfv
+#!/bin/bash
+
+# devstack/plugin.sh
+# Triggers specific functions to install and configure stx-nfv
+
+echo_summary "stx-nfv devstack plugin.sh called: $1/$2"
 
 # check for service enabled
 if is_service_enabled stx-nfv; then
     if [[ "$1" == "stack" && "$2" == "install" ]]; then
-	# Perform installation of service source
-	echo_summary "Installing stx-nfv"
+        # Perform installation of service source
+        echo_summary "Installing stx-nfv"
         install_guest_client
 
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
-	# Configure after the other layer 1 and 2 services have been configured
-	echo_summary "Configuring stx-nfv"
-	:
+    # Configure after the other layer 1 and 2 services have been configured
+    echo_summary "Configuring stx-nfv"
+    :
     fi
 
 fi
