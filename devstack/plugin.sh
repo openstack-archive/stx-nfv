db34b40fb4f5fc641249fb28b6d89ccecb3f2f2b
Dean Troyer <dtroyer@gmail.com>
2018-11-15 09:49:31 -0600

    Updates to DevStack plugin
    * Add a basic DevStack job to exercise this
    * Clean up plugin to match DevStack conventions
    
    Change-Id: I5b079eacebf9ddb433b5e831d557afe6a1be6880
    Signed-off-by: Dean Troyer <dtroyer@gmail.com>

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
-#
-# lib/stx-nfv
-# Functions to control the configuration and operation of the **network function virtualization** service
+
+# devstack/plugin.sh
+# Triggers specific functions to install and configure stx-nfv
+
+echo_summary "stx-nfv devstack plugin.sh called: $1/$2"
 
 # check for service enabled
 if is_service_enabled stx-nfv; then
     if [[ "$1" == "stack" && "$2" == "install" ]]; then
-	# Perform installation of service source
-	echo_summary "Installing stx-nfv"
+        # Perform installation of service source
+        echo_summary "Installing stx-nfv"
         install_nfv
 
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
-	# Configure after the other layer 1 and 2 services have been configured
-	echo_summary "Configuring stx-nfv"
-	:
+        # Configure after the other layer 1 and 2 services have been configured
+        echo_summary "Configuring stx-nfv"
+        :
     fi
     if [[ "$1" == "unstack" ]]; then
-	:
+        :
     fi
 
     if [[ "$1" == "clean" ]]; then
-	echo_summary "Clean stx-nfv"
-	clean_nfv
+        echo_summary "Clean stx-nfv"
+        cleanup_nfv
     fi
 fi
 
