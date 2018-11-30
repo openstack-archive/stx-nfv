39fe92a012d19ad8cffef54a4942dceba5d909dd
Yi Wang <yi.c.wang@intel.com>
2018-11-30 16:27:21 +0800

    Updates to stx-nfv Devstack plugin
    1. Refine nfv installation code to modify nfv config files
    2. Add code to start three nfv services: vim, vim-api and
       vim-webserver
    
    Story: 2003163
    Task: 23298
    
    Change-Id: Ic1e95dbaae4b319dcc9b4b20dea82bfb56e09696
    Signed-off-by: Yi Wang <yi.c.wang@intel.com>

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -8,22 +8,32 @@ echo_summary "stx-nfv devstack plugin.sh called: $1/$2"
 # check for service enabled
 if is_service_enabled stx-nfv; then
     if [[ "$1" == "stack" && "$2" == "install" ]]; then
-        # Perform installation of service source
         echo_summary "Installing stx-nfv"
-        install_nfv
+        install_stx_nfv
 
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
-        # Configure after the other layer 1 and 2 services have been configured
         echo_summary "Configuring stx-nfv"
-        :
-    fi
-    if [[ "$1" == "unstack" ]]; then
-        :
+        configure_stx_nfv
+
+    elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
+        echo_summary "Initializing stx-nfv"
+        start_stx_nfv
+
+    elif [[ "$1" == "stack" && "$2" == "test-config" ]]; then
+        echo_summary "Do sanity test on stx-nfv"
+        test_stx_nfv
     fi
 
+    # handle unstack mode
+    if [[ "$1" == "unstack" ]]; then
+        echo_summary "Shutdown stx-nfv services"
+        stop_stx_nfv
+    fi
+
+    # handle clean mode
     if [[ "$1" == "clean" ]]; then
-        echo_summary "Clean stx-nfv"
-        cleanup_nfv
+        echo_summary "Cleanup stx-nfv"
+        cleanup_stx_nfv
     fi
 fi
 
