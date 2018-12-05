cc5d379baffe09e414b22b6da969d03cfd5f39ff
Yi Wang <yi.c.wang@intel.com>
2018-12-05 18:04:56 +0800

    Some updates to stx-nfv Devstack plugin
    This installs, starts, stops, and clean nova-api-proxy service.
    
    Story: 2003163
    Task: 28141
    
    Change-Id: I6c48c21a97df0c9324e77f6ffff70b7afd0b57c9
    Signed-off-by: Yi Wang <yi.c.wang@intel.com>

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -13,6 +13,9 @@ if is_service_enabled stx-nfv; then
         if is_service_enabled nfv-vim || is_service_enabled nfv-vim-api || is_service_enabled nfv-vim-webserver; then
             install_nfv
         fi
+        if is_service_enabled nova-api-proxy; then
+            install_nova_api_proxy
+        fi
         if is_service_enabled guest-client; then
             install_guest_client
         fi
@@ -20,14 +23,18 @@ if is_service_enabled stx-nfv; then
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
         # Configure after the other layer 1 and 2 services have been configured
         echo_summary "Configuring stx-nfv"
-        :
+        if is_service_enabled nova-api-proxy; then
+            configure_nova_api_proxy
+        fi
 
     elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
         echo_summary "Starting stx-nfv"
         if is_service_enabled nfv-vim || is_service_enabled nfv-vim-api || is_service_enabled nfv-vim-webserver; then
             start_nfv
         fi
-
+        if is_service_enabled nova-api-proxy; then
+            start_nova_api_proxy
+        fi
     fi
 
     if [[ "$1" == "unstack" ]]; then
@@ -35,12 +42,18 @@ if is_service_enabled stx-nfv; then
         if is_service_enabled nfv-vim || is_service_enabled nfv-vim-api || is_service_enabled nfv-vim-webserver; then
             stop_nfv
         fi
+        if is_service_enabled nova-api-proxy; then
+            stop_nova_api_proxy
+        fi
     fi
 
     if [[ "$1" == "clean" ]]; then
         echo_summary "Clean stx-nfv"
         if is_service_enabled nfv-vim || is_service_enabled nfv-vim-api || is_service_enabled nfv-vim-webserver; then
             cleanup_nfv
+        fi
+        if is_service_enabled nova-api-proxy; then
+            cleanup_nova_api_proxy
         fi
         if is_service_enabled guest-client; then
             cleanup_guest_client
