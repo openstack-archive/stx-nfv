81982eacb5e414d186044e9c15ace32a12dd83b0
Yi Wang <yi.c.wang@intel.com>
2018-12-14 15:38:46 +0800

    updates to stx-nfv devstack plugin
    1. bring up guestAgent service
    2. bring up guestServer service
    3. move service enable check from plugin.sh to lib/stx-nfv
    
    Story: 2003163
    Task: 29191
    
    Change-Id: I51badd4a737b0f4e2ed66f926640ad202bf6fb5f
    Signed-off-by: Yi Wang <yi.c.wang@intel.com>

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -10,56 +10,37 @@ if is_service_enabled stx-nfv; then
     if [[ "$1" == "stack" && "$2" == "install" ]]; then
         # Perform installation of service source
         echo_summary "Installing stx-nfv"
-        if is_service_enabled nfv-vim || is_service_enabled nfv-vim-api || is_service_enabled nfv-vim-webserver; then
-            install_nfv
-        fi
-        if is_service_enabled nova-api-proxy; then
-            install_nova_api_proxy
-        fi
-        if is_service_enabled guest-client; then
-            install_guest_client
-        fi
+        install_nfv
+        install_nova_api_proxy
+        install_guest_client
+        install_mtce_guest
 
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
         # Configure after the other layer 1 and 2 services have been configured
         echo_summary "Configuring stx-nfv"
-        if is_service_enabled nfv-vim || is_service_enabled nfv-vim-api || is_service_enabled nfv-vim-webserver; then
-            configure_nfv
-        fi
-        if is_service_enabled nova-api-proxy; then
-            configure_nova_api_proxy
-        fi
+        configure_nfv
+        configure_nova_api_proxy
+        configure_mtce_guest
 
     elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
         echo_summary "Starting stx-nfv"
-        if is_service_enabled nfv-vim || is_service_enabled nfv-vim-api || is_service_enabled nfv-vim-webserver; then
-            start_nfv
-        fi
-        if is_service_enabled nova-api-proxy; then
-            start_nova_api_proxy
-        fi
+        start_nfv
+        start_nova_api_proxy
+        start_mtce_guest
     fi
 
     if [[ "$1" == "unstack" ]]; then
         echo_summary "Shutdown stx-nfv"
-        if is_service_enabled nfv-vim || is_service_enabled nfv-vim-api || is_service_enabled nfv-vim-webserver; then
-            stop_nfv
-        fi
-        if is_service_enabled nova-api-proxy; then
-            stop_nova_api_proxy
-        fi
+        stop_nfv
+        stop_nova_api_proxy
+        stop_mtce_guest
     fi
 
     if [[ "$1" == "clean" ]]; then
         echo_summary "Clean stx-nfv"
-        if is_service_enabled nfv-vim || is_service_enabled nfv-vim-api || is_service_enabled nfv-vim-webserver; then
-            cleanup_nfv
-        fi
-        if is_service_enabled nova-api-proxy; then
-            cleanup_nova_api_proxy
-        fi
-        if is_service_enabled guest-client; then
-            cleanup_guest_client
-        fi
+        cleanup_nfv
+        cleanup_nova_api_proxy
+        cleanup_guest_client
+        cleanup_mtce_guest
     fi
 fi
