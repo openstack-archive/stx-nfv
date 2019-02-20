752cbc30396aceacbeedb42ce5ad0570c145cbb3
Dean Troyer <dtroyer@gmail.com>
2019-02-20 16:32:47 -0600

    DevStack plugin updates for bionic job
    This makes some cleanup changes to the DevStack plugin for the
    change to master and bionic.
    * Define values for precedence handling by the upstream devstack playbook
    * Add STX_INST_DIR for a deterministic install location
    * Add stx-metal to required plugins
    * Consolidate all of the is_enabled() handling in the main plugin
      entrypoint functions in lib/stx-nfv
    
    Depends-On: https://review.openstack.org/639858
    Change-Id: If9fdc8221443561ed489c38b9006de2f6e9755d7
    Signed-off-by: Dean Troyer <dtroyer@gmail.com>

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -11,36 +11,24 @@ if is_service_enabled stx-nfv; then
         # Perform installation of service source
         echo_summary "Installing stx-nfv"
         install_nfv
-        install_nova_api_proxy
-        install_guest_client
-        install_mtce_guest
 
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
         # Configure after the other layer 1 and 2 services have been configured
         echo_summary "Configuring stx-nfv"
         configure_nfv
-        configure_nova_api_proxy
-        configure_mtce_guest
 
     elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
         echo_summary "Starting stx-nfv"
         start_nfv
-        start_nova_api_proxy
-        start_mtce_guest
     fi
 
     if [[ "$1" == "unstack" ]]; then
         echo_summary "Shutdown stx-nfv"
         stop_nfv
-        stop_nova_api_proxy
-        stop_mtce_guest
     fi
 
     if [[ "$1" == "clean" ]]; then
         echo_summary "Clean stx-nfv"
         cleanup_nfv
-        cleanup_nova_api_proxy
-        cleanup_guest_client
-        cleanup_mtce_guest
     fi
 fi
